e0ba6183cea2fdf79a679b6cc2d56c7da35e377d
Soulaimane Ouhmida <105684836+Soulaimane07@users.noreply.github.com>
2024-08-09 12:09:06 +0200

    66

--- front/src/Interfaces/PreLogged.js
+++ front/src/Interfaces/PreLogged.js
@@ -1,11 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import Footer from '../Components.js/Footer'
-import { GetUserProfiles } from '../Components.js/Functions'
-import { useDispatch, useSelector } from 'react-redux'
-import { setProfilee } from '../Components.js/Redux/Slices/UserSlice'
-import { getProfile } from '../Components.js/Redux/Slices/ProfileSlice'
-import { GoPlus } from 'react-icons/go'
-import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import AddProfile from '../Pages/User/Profile/AddProfile'
 import Hello from './Hello'
 
