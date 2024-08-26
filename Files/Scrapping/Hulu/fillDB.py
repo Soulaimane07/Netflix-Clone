ab0495970d77e7854b9f71bdf1a05382a028960d
Soulaimane Ouhmida <105684836+Soulaimane07@users.noreply.github.com>
2024-08-26 17:12:51 +0200

    99

--- Files/Scrapping/Hulu/fillDB.py
+++ Files/Scrapping/Hulu/fillDB.py
@@ -2,12 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import mysql.connector
 
+
+
+
+
+
+
+
+
+
+
 def connect_to_mysqlDB():
     config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'localhost',
-        'database': 'netflix'
+        'user': 'admin',
+        'password': 'password1234',
+        'host': 'netflix-relational.cjqo6ywc0hfl.eu-west-3.rds.amazonaws.com',
+        'database': 'netflix',
+        'port': 3306
     }
     try:
         cnx = mysql.connector.connect(**config)
@@ -16,6 +27,14 @@ def connect_to_mysqlDB():
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None, None
+
+
+
+
+
+
+
+
 
 def insert_Data(data):
     cursor, cnx = connect_to_mysqlDB()
@@ -43,25 +62,31 @@ def insert_Data(data):
         cursor.close()
         cnx.close()
 
+
+
+
+
+
+
+
 def ScrappingData(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         movie_items = soup.find_all('div', class_='GridCollection__item')
+        
+
         for movie in movie_items:
             title_element = movie.find('span', class_='css-1c0j34c')
             title = title_element.text.strip() if title_element else 'No Title'
             
-            cardimage = movie.find('img')
+            cardimage = movie.find('img', class_='Tile__image')
             cardimage_src = cardimage.get('data-src') if cardimage else None
 
-            details_element = movie.find('a')
+            details_element = movie.find('a', class_='Tile__thumbnail')
             details_url = details_element.get('href') if details_element else None
 
-            if "data:" in cardimage_src:
-                continue 
-            
-            if "series" in details_url: 
+            if True: 
                 newName = title.replace(' ', '+')
                 
                 ScrappingDetails({
@@ -71,11 +96,18 @@ def ScrappingData(url):
     else:
         print(f'Failed to retrieve the webpage: {response.status_code}')
 
+
+
+
+
+
+
+
 def ScrappingDetails(data, url, newName):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        moviedetails = soup.find('div', class_='DetailEntityMasthead cu-masthead DetailEntityMasthead--with-ribbon')
+        moviedetails = soup.find('div', class_='DetailEntityMasthead')
         
         description_element = moviedetails.find('span', class_="DetailEntityMasthead__description-truncated css-18868ls")
         description = description_element.get_text(strip=True) if description_element else None
@@ -91,23 +123,14 @@ def ScrappingDetails(data, url, newName):
             "cardimage": "https://netflix-movies-series.s3.eu-west-3.amazonaws.com/series/card/" + newName + ".jpg",
             "logoimage": "https://netflix-movies-series.s3.eu-west-3.amazonaws.com/series/logo/" + newName + ".jpg",
             "trailer": "https://netflix-movies-series.s3.eu-west-3.amazonaws.com/series/trailer/" + newName + ".mp4",
-            "video": "https://netflix-movies-series.s3.eu-west-3.amazonaws.com/series/video/" + newName + ".mp4",
             "description": description[:200],
-            "rating": "5+",
+            "rating": "18+",
             "title": data["name"],
             "year": year,
             "seasons": 0,
-            "network": 5, 
+            "network": 7, 
             "genres": [
-                {
-                    "id": 15
-                },
-                {
-                    "id": 14
-                },
-                {
-                    "id": 13
-                }
+                
             ]
         }
 
@@ -117,6 +140,13 @@ def ScrappingDetails(data, url, newName):
     else:
         print(f'Failed to retrieve the webpage: {response.status_code}')
 
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    url = 'https://www.hulu.com/network/cartoon-network-84334a00-8787-41c2-8be9-e0a3922edd7c'
+    url = 'https://www.hulu.com/hub/tv/collections/8'
     ScrappingData(url)
